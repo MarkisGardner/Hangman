11e8f6cf31291ee1b03ae7631c7d93452dfa4ebe
MarkisGardner <109382615+MarkisGardner@users.noreply.github.com>
2024-07-13 01:34:41 -0400

    Finalized program.

--- main.py
+++ main.py
@@ -87,12 +87,12 @@ def playgame(animals):
 
 # print("Welcome to Hangman!")
 animals = ReadDataFromCSV.load_data()
-again=True
+again = True
 while again:
     playgame(animals)
     ans = input("Would you like to play again? (y/n): ")
     if ans.lower() == 'y' or ans.lower() == 'yes' or ans.upper() == 'YES' or ans.upper() == 'Y':
-        again=True
+        again = True
     else:
-        again=False
+        again = False
         print("Thanks for playing!")
